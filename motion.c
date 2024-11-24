e6d96063b49c8f9ff6a820cf040cbcae4520f2ee
jaeheon01 <164356391+jaeheon01@users.noreply.github.com>
2024-11-24 16:36:40 +0900

    Jaeheon (#2)
    * vibration sensor
    
    * motion sensor c code

---
+++ motion.c
@@ -0,0 +1,134 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+
+#define IN 0
+#define OUT 1
+#define LOW 0
+#define HIGH 1
+#define PIR_PIN 22
+#define VALUE_MAX 40
+#define DIRECTION_MAX 128
+
+static int GPIOExport(int pin) {
+#define BUFFER_MAX 3
+	char buffer[BUFFER_MAX];
+	ssize_t bytes_written;
+	int fd;
+	
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	if(-1 == fd) {
+		fprintf(stderr, "Failed to open for writing!\n");
+		return -1;
+	}
+	
+	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+	write(fd, buffer, bytes_written);
+	close(fd);
+	return 0;
+}
+
+static int GPIODirection (int pin, int dir){
+	static const char s_directions_str[]="int\0out";
+	char path[DIRECTION_MAX];
+	int fd;
+	
+	snprintf(path,DIRECTION_MAX, "/sys/class/gpio/gpio%d/direction", pin);
+	fd = open(path, O_WRONLY);
+	if(-1 == fd) {
+		fprintf(stderr, "Failed to open gpio direction for writing!\n");
+		return -1;
+	}
+	if (-1 == 
+		write(fd, &s_directions_str[IN == dir ? 0 : 3], IN ==dir ? 2 : 3)) {
+			fprintf(stderr, "Failed to set direction!\n");
+			return -1;
+	}
+	
+	close(fd);
+	return 0;
+}
+
+static int GPIORead(int pin){
+	char path[DIRECTION_MAX];
+	char value_str[3];
+	int fd;
+	
+	snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value",pin);
+	fd = open(path, O_RDONLY);
+	if(fd == -1){
+		perror("Error: GPIO read");
+		return -1;
+	}
+	
+	read(fd, value_str,3);
+	close(fd);
+	
+	return(value_str[0] == '1') ? HIGH : LOW;
+}
+
+
+static int GPIOWrite(int pin, int value){
+	static const char s_values_str[] = "01";
+	
+	char path[VALUE_MAX];
+	int fd;
+	
+	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	fd = open(path,O_WRONLY);
+	if(-1 == fd) {
+		fprintf(stderr, "Failed to open gpio value for writing!\n");
+		return -1;
+	}
+	
+	if(1 != write(fd, &s_values_str[LOW == value ? 0: 1],1)) {
+		fprintf(stderr, "Failed to write value!\n");
+		return -1;
+	}
+	close(fd);
+	return 0;
+}
+
+static int GPIOUnexport(int pin){
+	char buffer[BUFFER_MAX];
+	ssize_t bytes_written;
+	int fd;
+	
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	if(-1 == fd){
+		fprintf(stderr, "Failed to open unexport for writing!\n");
+		return -1;
+	}
+	
+	bytes_written = snprintf(buffer, BUFFER_MAX, "%d", pin);
+	write(fd, buffer, bytes_written);
+	close(fd);
+	return 0;
+}
+
+int main() {
+	if (GPIOExport(PIR_PIN) == -1) return 1;
+	if (GPIODirection(PIR_PIN, IN) == -1) return 2;
+	
+	while(1) {
+		int motionDetected = GPIORead(PIR_PIN);
+		if (motionDetected == HIGH) {
+			printf("Motion detected!\n");
+		} else {
+			printf("No motion detected\n");
+		}
+		usleep(200* 1000);
+	}
+	
+	if(GPIOUnexport(PIR_PIN) == -1){ 
+		return 3;
+	}
+	return 0;
+}
+
