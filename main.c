5f7a99c5cf5833c39c57ac63054fb71f0ec08ae5
meansash <meansash@naver.com>
2024-12-02 20:00:04 +0900

    Integrate camera module with motion and vibration sensor logic - Added camera.c and camera.h to handle image capture using libcamera - Updated main.c to call captureImage() when mothin and vibration sensors detect - Reset sensor states and modify a delay after processing sensor events

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "shared.h"
+#include "camera.h"
 
 
 int vibration_detected = 0;
@@ -14,23 +15,28 @@ int step = 0;
 
 int main(){
     pthread_t vibration_thread, motion_thread;
-
+    // 진동 센서 스레드 생성
     if(pthread_create(&vibration_thread, NULL, vibration_sensor, NULL)){
         fprintf(stderr, "Error creating vibration sensor thread\n");
         return 1;
+
+    // 모션 센서 스레드 생성
     }
     if(pthread_create(&motion_thread, NULL, motion_sensor, NULL)){
         fprintf(stderr, "Error creating motion sensor thread\n");
         return 2;
     }
 
+    // 센서 감지 루프
     while(1){
 		pthread_mutex_lock(&lock);
-		while(step!=2){
+		while(step!=2){ // 두 센서가 모두 감지될 때까지 대기
 			pthread_cond_wait(&cond,&lock);
 		}
         if(vibration_detected && motion_detected){
-            printf("[Main] Invade detected by both sensor!\n"); 
+            printf("[Main] Invade detected by both sensors!\n"); 
+            // Image Capture
+            captureImage();
         }
         else{
             printf("[Main] No Invade detected!\n");         
@@ -40,7 +46,8 @@ int main(){
         step = 0;
         pthread_cond_broadcast(&cond);
         pthread_mutex_unlock(&lock);
-        sleep(1);
+        // 30초 대기
+        sleep(30);
     }
 
     pthread_join(vibration_thread,NULL); 
