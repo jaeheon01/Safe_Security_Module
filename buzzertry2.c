78a0978bb22a881aaa94621017ec0d292dcb3f5b
NayeongS <128385133+NayeongS@users.noreply.github.com>
2024-12-04 14:13:29 +0900

    Update buzzertry2.c

--- buzzertry2.c
+++ buzzertry2.c
@@ -1,3 +1,42 @@
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "shared.h"
+
+#define OUT 1
+#define HIGH 1
+#define LOW 0
+#define VALUE_MAX 40
+#define DIRECTION_MAX 128
+#define BUZZER_PIN 16
+
+// buzzer initialize
+int buzzer_init() {
+    if (GPIOExport(BUZZER_PIN) == -1) {
+        fprintf(stderr, "[BUZZER] Failed to export GPIO pin.\n");
+        return -1;
+    }
+    if (GPIODirection(BUZZER_PIN, OUT) == -1) {
+        fprintf(stderr, "[BUZZER] Failed to set GPIO direction.\n");
+        return -1;
+    }
+    return 0;
+}
+
+int buzzer_on() {
+    return GPIOWrite(BUZZER_PIN, HIGH);
+}
+int buzzer_off() {
+    return GPIOWrite(BUZZER_PIN, LOW);
+}
+int buzzer_cleanup() {
+    return GPIOUnexport(BUZZER_PIN);
+}
+
+// Buzzer thread
 void* buzzer_thread(void* arg) {
     buzzer_init();
     while (1) {
